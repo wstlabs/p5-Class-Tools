7c6a0f02099d098e9d2cc9f2229f5c3e6c2a6190
wstlabs <nobody@devnull.org>
2012-03-06 18:17:29 +0000

    added find/get version methods; use ext mods for heavy lifting

--- lib/Class/Tools.pm
+++ lib/Class/Tools.pm
@@ -6,29 +6,44 @@ use warnings;
 use strict;
 use Carp;
 use Scalar::Util qw( reftype );
+use Class::Inspector;
+use Module::Load; 
+use Class::Unload;
 use Exporter::Tidy
     -default => [qw|
-        is_loaded
         is_valid_class_name
+        get_package_version
+        find_package_version
         find_parent_classes 
+    |],
+    -deprecated => [qw|
+        is_loaded
     |]
 ;
 
-our $VERSION = '0.001';
-
-#
-# similar to Class::MOP::is_class_loaded, but a bit simpler 
-sub is_loaded  {
-    my $class = shift // return '';
-    no strict 'refs';
-    scalar keys %{"$class\::"} ? 1 : '' 
-}
+our $VERSION = '0.002';
 
 sub is_valid_class_name {
     my $class = shift;
     defined $class && $class =~ m{\A \w+(?:::\w+)* \z}xms
 }
 
+sub get_package_version  {
+    my $package = shift // confess "need a package name";
+    no strict 'refs';
+    ${"$package\::VERSION"};
+}
+
+sub find_package_version  {
+    my $package = shift // confess "need a package name";
+    Class::Inspector->loaded($package) && get_package_version($package);
+    Class::Inspector->installed($package) || 
+        confess "package '$package' not installed";
+    load $package;
+    my $version = get_package_version($package);
+    Class::Unload->unload($package);
+    return $version
+}
 
 # simply returns the upward inheritance stack of a given package.
 # linear inheritance only, please.
@@ -45,6 +60,19 @@ sub find_parent_classes  {
         ( $parent, find_parent_classes($parent) ) :
     ()
 }
+
+#
+# deprecated.  use one of either
+#
+#   Class::MOP::is_class_loaded
+#   Class::Inspector->loaded
+#
+# instead.
+sub is_loaded  { Class::Inspector->loaded(@_) }
+# my $class = shift // return '';
+# no strict 'refs';
+# scalar keys %{"$class\::"} ? 1 : '' 
+
 
 __END__
 
